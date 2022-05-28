ce0b43a807b4cfda5d4e435a68614eb7852ffec2
Tomas Kraus <tomas.krausik@gmail.com>
2022-05-29 01:42:24 +0200

    rename credentials fields
    to match those in the http request header

--- src/middlewares/rossum-credentials-manager.js
+++ src/middlewares/rossum-credentials-manager.js
@@ -7,14 +7,14 @@ const assert = require('node:assert/strict');
 const create = logger => {
     return {
         /**
-         * returns { userName: string, password: string }
+         * returns { username: string, password: string }
          */
         getCredentials: () => {
             assert.notEqual(process.env.ROSSUM_USER_NAME, undefined, "ROSSUM_USER_NAME env variable is mandatory to run this app")
-            const userName = process.env.ROSSUM_USER_NAME;
+            const username = process.env.ROSSUM_USER_NAME;
             assert.notEqual(process.env.ROSSUM_PASSWORD, undefined, "ROSSUM_PASSWORD env variable is mandatory to run this app")
             const password = process.env.ROSSUM_PASSWORD;
-            const credentials = { userName, password }
+            const credentials = { username, password }
 
             logger.debug('Credentials sets successfully')
             return credentials
