eef87bea17fb814893e79c86e1a140c8ef58f4f0
Tomas Kraus <tomas.krausik@gmail.com>
2022-05-28 16:07:31 +0200

    simple server running
    with credentials prepared

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
+/**
+ * main module 
+ */
+
+
 const http = require('http')
+const winston = require('winston');
 
-const port = process.env.PORT || 3000
+const logger = winston.createLogger({
+    level: 'debug',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'logs/combined.log' }),
+    ],
+});
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  console.log("hit")
-  res.setHeader('Content-Type', 'text/html')
-  res.end('<h1>Hello, World!</h1>')
-})
+try {
+    const credentialsManager = require("./src/middlewares/rossum-credentials-manager").create(logger);
+    credentialsManager.getCredentials();
 
-server.listen(port, () => {
-  console.log(`Server running at port ${port}`)
-})
+
+    const port = process.env.PORT || 3000
+
+    const server = http.createServer((req, res) => {
+        res.statusCode = 200
+        logger.debug("hit")
+        res.setHeader('Content-Type', 'text/html')
+
+        res.end('<h1>Hello, World!</h1>')
+
+    })
+
+    server.listen(port, () => {
+        logger.info(`Server running at port ${port}`)
+    })
+
+} catch (err) {
+    logger.error(err.message)
+}
