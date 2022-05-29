4503292e3521aa738230c35b98904b31e2425e2c
Tomas Kraus <tomas.krausik@gmail.com>
2022-05-29 21:48:05 +0200

    remove comment

--- src/services/rossum-service.js
+++ src/services/rossum-service.js
@@ -6,11 +6,6 @@
 const axios = require('axios');
 
 
-// makes more sense to me, not to place this to the separate config file
-// const rossumReq = axios.create({
-//     baseUrl: "https://elis.rossum.ai/api/"
-// })
-
 const _ROSSUM_REQUEST_CONFIG = {
     baseURL: "https://elis.rossum.ai/api"
 }
