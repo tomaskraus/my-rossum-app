609ad78d07c8def4646ce3926a5ce82baa05b605
Tomas Kraus <tomas.krausik@gmail.com>
2022-06-04 21:18:20 +0200

    use custom authorizer
    to log incorrect credentials usage

--- src/middlewares/app-auth-manager.js
+++ src/middlewares/app-auth-manager.js
@@ -13,15 +13,25 @@ const create = logger => {
   assert.notEqual(process.env.APP_PASSWORD, undefined, 'APP_PASSWORD env variable is mandatory to run this app')
   const password = process.env.APP_PASSWORD
   logger.debug('App credentials sets successfully')
+
+  const customAuthorizer = (usrName, passw) => {
+    const userMatches = basicAuth.safeCompare(usrName, username)
+    const passwordMatches = basicAuth.safeCompare(passw, password)
+    if (userMatches & passwordMatches) {
+      logger.debug('AppAuthManager authorizer: credentials OK')
+      return true
+    }
+    logger.warn('AppAuthManager authorizer: wrong credentials')
+    return false
+  }
+
   return {
 
     /**
      * returns Express request handler for user authorization
      */
     getRequestHandler: () => {
-      const userRecords = {}
-      userRecords[username] = password
-      return basicAuth({ users: userRecords })
+      return basicAuth({ authorizer: customAuthorizer })
     }
   }
 }
